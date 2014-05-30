babcb801d5b60ad8737ce67eb79fd389fbedbf65
David McCuskey <my-github@davidmccuskey.com>
2014-05-30 10:53:55 -0600

    add hex dump function

--- lua_utils.lua
+++ lua_utils.lua
@@ -302,6 +302,19 @@ function Utils.getUniqueRandom( include, exclude )
 end
 
 
+-- hexDump()
+-- pretty-print data in hex table
+--
+function Utils.hexDump( buf )
+	for i=1,math.ceil(#buf/16) * 16 do
+		if (i-1) % 16 == 0 then io.write(string.format('%08X  ', i-1)) end
+		io.write( i > #buf and '   ' or string.format('%02X ', buf:byte(i)) )
+		if i %  8 == 0 then io.write(' ') end
+		if i % 16 == 0 then io.write( buf:sub(i-16+1, i):gsub('%c','.'), '\n' ) end
+	end
+end
+
+
 
 --====================================================================--
 -- String Functions
