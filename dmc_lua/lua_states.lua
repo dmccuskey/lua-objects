df18b1e1f6d67a96547f48d94f53fd853d0c238a
David McCuskey <my-github@davidmccuskey.com>
2014-07-03 17:11:26 -0600

    add tests for lua_states, tidy up code

--- dmc_lua/lua_states.lua
+++ dmc_lua/lua_states.lua
@@ -37,100 +37,140 @@ DEALINGS IN THE SOFTWARE.
 
 -- Semantic Versioning Specification: http://semver.org/
 
-local VERSION = "1.0.1"
+local VERSION = "1.1.0"
 
 
 --====================================================================--
--- Setup, Constants
+-- Support Functions
 
-local tinsert = table.insert
-local tremove = table.remove
 
+function outStr( msg )
+	return "Lua States :: " .. tostring( msg )
+end
+
+function errStr( msg )
+	return "\n\nERROR: Lua States :: " .. tostring( msg ) .. "\n\n"
+end
 
 
 --====================================================================--
 -- States Container
 --====================================================================--
 
-local States = {}
 
-States._DEBUG = false
+local States = {}
 
 
 --== State API Methods ==--
 
 
-function States._setState( self, state )
-	if States._DEBUG then
-		print( "Lua States::setState: is now >> " .. tostring( state ) )
-	end
-
-	local f = self[ state ]
-
-	assert( type(f)=='function', "\n\nERROR: Lua States :: missing state method '" .. tostring( state ) .. "'\n\n" )
-
-	self._curr_state = f
-	self._curr_state_name = state
-end
-
-
-function States._gotoState( self, state, ... )
-	if States._DEBUG then
-		print( "Lua States::gotoState: " .. tostring( state ) )
-	end
-
-	tinsert( self._state_stack, 1, self._curr_state_name )
-	self:_curr_state( state, ... )
-end
-
-
-function States._gotoPreviousState( self, ... )
-	local state = tremove( self._state_stack, 1 )
-	if States._DEBUG then
-		print( "Lua States::gotoPreviousState: going to >> " .. tostring( state ) )
-	end
-
-	self:_curr_state( state, ... )
-end
-
-
 function States._getState( self )
-	return self._curr_state_name
+	local vars = self.__lua_states
+	return vars.curr_state_name
+end
+
+function States._setState( self, state_name )
+	local vars = self.__lua_states
+
+	assert( state_name, errStr("missing state name") )
+	assert( type(state_name)=='string', errStr("state name must be string'" .. tostring( state_name ) ) )
+
+	if vars.debug_on then
+		print( outStr("setState: is now '" .. tostring( state_name ) .. "'" ) )
+	end
+	local f = self[ state_name ]
+
+	assert( type(f)=='function', errStr("missing method for state name: '" .. tostring( state_name ) ) )
+
+	vars.curr_state_func = f
+	vars.curr_state_name = state_name
+end
+
+function States._gotoState( self, state_name, ... )
+	local vars = self.__lua_states
+
+	assert( state_name, errStr("no state name given") )
+	assert( vars.curr_state_func, errStr("no initial state method") )
+
+	if vars.debug_on then
+		print( outStr( "gotoState: '" .. vars.curr_state_name .. "' >> '".. tostring( state_name ) .. "'" ) )
+	end
+
+	self:pushStateStack( vars.curr_state_name )
+	vars.curr_state_func( self, state_name, ... )
 end
 
 
 function States._getPreviousState( self )
-	return self._state_stack[1]
+	local vars = self.__lua_states
+
+	assert( #vars.state_stack > 0, errStr("state stack is empty") )
+
+	return vars.state_stack[1]
+end
+
+function States._gotoPreviousState( self, ... )
+	local vars = self.__lua_states
+	local state_name = self:popStateStack()
+
+	assert( state_name, errStr("no state name given") )
+	assert( vars.curr_state_func, errStr("no initial state method") )
+
+	if vars.debug_on then
+		print( outStr( "gotoPreviousState: going to >> " .. tostring( state_name ) ) )
+	end
+
+	vars.curr_state_func( self, state_name, ... )
 end
 
 
-function States._pushState( self, state_name )
-	tinsert( self._state_stack, 1, state_name )
+function States._pushStateStack( self, state_name )
+	local vars = self.__lua_states
+
+	assert( state_name, errStr("no state name given") )
+
+	table.insert( vars.state_stack, 1, state_name )
+end
+
+function States._popStateStack( self )
+	local vars = self.__lua_states
+
+	assert( #vars.state_stack > 0, errStr("state stack is empty") )
+
+	return table.remove( vars.state_stack, 1 )
 end
 
 
 function States._resetStates( self )
-	if States._DEBUG then
-		print( "Lua States::resetStates" )
+	local vars = self.__lua_states
+	if vars and vars.debug_on then
+		print( outStr("resetStates: resetting object states") )
 	end
-	self._state_stack = {}
-	self._curr_state = nil
-	self._curr_state_name = ""
+	self.__lua_states = {
+		state_stack = {},
+		curr_state_func = nil,
+		curr_state_name = "",
+		debug_on = false
+	}
+end
+
+
+function States._setDebug( self, value )
+	local vars = self.__lua_states
+	vars.debug_on = value
+end
+
+
+
+-- private method, for testing
+function States._stateStackSize( self )
+	return #self.__lua_states.state_stack
 end
 
 
 --== Facade API Methods ==--
 
-
-function States._setDebug( value )
-	States._DEBUG = value
-end
-
-
 function States._mixin( obj )
-	if States._DEBUG then
-		print( "Lua States::mixin: ", obj )
-	end
 
 	obj = obj or {}
 
@@ -138,13 +178,18 @@ function States._mixin( obj )
 	States._resetStates( obj )
 
 	-- add methods
+	obj.getState = States._getState
 	obj.setState = States._setState
 	obj.gotoState = States._gotoState
-	obj.gotoPreviousState = States._gotoPreviousState
-	obj.getState = States._getState
 	obj.getPreviousState = States._getPreviousState
-	obj.pushState = States._pushState
+	obj.gotoPreviousState = States._gotoPreviousState
+	obj.pushStateStack = States._pushStateStack
+	obj.popStateStack = States._popStateStack
 	obj.resetStates = States._resetStates
+	obj.setDebug = States._setDebug
+
+	-- private method, for testing
+	obj._stateStackSize = States._stateStackSize
 
 	return obj
 end
@@ -157,7 +202,6 @@ end
 --====================================================================--
 
 return {
-	setDebug = States._setDebug
 	mixin = States._mixin
 }
 
