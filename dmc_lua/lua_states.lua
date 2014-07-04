1742d57c670fb9f42aa5ff3a09f31c5c3cd00edf
David McCuskey <my-github@davidmccuskey.com>
2014-07-04 16:10:28 -0700

    change storage of state vars

--- dmc_lua/lua_states.lua
+++ dmc_lua/lua_states.lua
@@ -65,106 +65,90 @@ local States = {}
 
 
 function States._getState( self )
-	local vars = self.__lua_states
-	return vars.curr_state_name
+	return self.__curr_state_name
 end
 
 function States._setState( self, state_name )
-	local vars = self.__lua_states
-
 	assert( state_name, errStr("missing state name") )
 	assert( type(state_name)=='string', errStr("state name must be string'" .. tostring( state_name ) ) )
 
-	if vars.debug_on then
+	if self.__debug_on then
 		print( outStr("setState: is now '" .. tostring( state_name ) .. "'" ) )
 	end
 	local f = self[ state_name ]
 
 	assert( type(f)=='function', errStr("missing method for state name: '" .. tostring( state_name ) ) )
 
-	vars.curr_state_func = f
-	vars.curr_state_name = state_name
+	self.__curr_state_func = f
+	self.__curr_state_name = state_name
 end
 
 function States._gotoState( self, state_name, ... )
-	local vars = self.__lua_states
-
 	assert( state_name, errStr("no state name given") )
-	assert( vars.curr_state_func, errStr("no initial state method") )
+	assert( self.__curr_state_func, errStr("no initial state method") )
 
-	if vars.debug_on then
-		print( outStr( "gotoState: '" .. vars.curr_state_name .. "' >> '".. tostring( state_name ) .. "'" ) )
+	if self.__debug_on then
+		print( outStr( "gotoState: '" .. self.__curr_state_name .. "' >> '".. tostring( state_name ) .. "'" ) )
 	end
 
-	self:pushStateStack( vars.curr_state_name )
-	vars.curr_state_func( self, state_name, ... )
+	self:pushStateStack( self.__curr_state_name )
+	self.__curr_state_func( self, state_name, ... )
 end
 
 
 function States._getPreviousState( self )
-	local vars = self.__lua_states
+	assert( #self.__state_stack > 0, errStr("state stack is empty") )
 
-	assert( #vars.state_stack > 0, errStr("state stack is empty") )
-
-	return vars.state_stack[1]
+	return self.__state_stack[1]
 end
 
 function States._gotoPreviousState( self, ... )
-	local vars = self.__lua_states
 	local state_name = self:popStateStack()
 
 	assert( state_name, errStr("no state name given") )
-	assert( vars.curr_state_func, errStr("no initial state method") )
+	assert( self.__curr_state_func, errStr("no initial state method") )
 
-	if vars.debug_on then
+	if self.__debug_on then
 		print( outStr( "gotoPreviousState: going to >> " .. tostring( state_name ) ) )
 	end
 
-	vars.curr_state_func( self, state_name, ... )
+	self.__curr_state_func( self, state_name, ... )
 end
 
 
 function States._pushStateStack( self, state_name )
-	local vars = self.__lua_states
-
 	assert( state_name, errStr("no state name given") )
 
-	table.insert( vars.state_stack, 1, state_name )
+	table.insert( self.__state_stack, 1, state_name )
 end
 
 function States._popStateStack( self )
-	local vars = self.__lua_states
+	assert( #self.__state_stack > 0, errStr("state stack is empty") )
 
-	assert( #vars.state_stack > 0, errStr("state stack is empty") )
-
-	return table.remove( vars.state_stack, 1 )
+	return table.remove( self.__state_stack, 1 )
 end
 
 
 function States._resetStates( self )
-	local vars = self.__lua_states
-	if vars and vars.debug_on then
+	if self.__debug_on then
 		print( outStr("resetStates: resetting object states") )
 	end
-	self.__lua_states = {
-		state_stack = {},
-		curr_state_func = nil,
-		curr_state_name = "",
-		debug_on = false
-	}
+	self.__state_stack = {}
+	self.__curr_state_func = nil
+	self.__curr_state_name = ""
+	self.__debug_on = false
 end
 
 
 function States._setDebug( self, value )
-	local vars = self.__lua_states
-	vars.debug_on = value
+	self.__debug_on = value
 end
 
 
 
 -- private method, for testing
 function States._stateStackSize( self )
-	return #self.__lua_states.state_stack
+	return #self.__state_stack
 end
 
 
