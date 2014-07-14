d25b8bfb83a5b2dd69c6a7c88ee14e7021dc15c8
David McCuskey <my-github@davidmccuskey.com>
2014-07-14 11:27:14 -0600

    add output to assert

--- dmc_lua/lua_objects.lua
+++ dmc_lua/lua_objects.lua
@@ -60,7 +60,7 @@ local Utils = require 'lua_utils'
 local CONSTRUCTOR_FUNC_NAME = 'new'
 
 local function setConstructorName( name )
-	assert( type(name)=='string' )
+	assert( type(name)=='string', "expected string for constructor name" )
 	CONSTRUCTOR_FUNC_NAME = name
 end
 
